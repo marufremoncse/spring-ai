8792413d91b87136bc7996c46c20227dc5adba97
marufremoncse <marufremon.cse14.2@gmail.com>
2024-10-22 08:33:36 +0600

    gitignore

---
+++ src/main/java/com/codingsense/springai/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.codingsense.springai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	@Bean
+	public 
+}
